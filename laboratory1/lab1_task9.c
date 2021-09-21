26d99315ecd2024596c1711edf71a3bae6912237
Stanislau Marudau <marudov.stas@gmail.com>
2021-09-21 17:29:19 +0300

    Code changes

--- laboratory1/lab1_task9.c
+++ laboratory1/lab1_task9.c
@@ -1,4 +1,6 @@
-
+// Write a program that requests the user to enter a Fahrenheit temperature.
+// The program should read the temperature as a type double number and pass it as an argument to a user-supplied function called Temperatures(). This function should calculate the Celsius equivalent and the Kelvin equivalent and display all three temperatures with a precision of two places to the right of the decimal.
+// It should identify each value with the temperature scale it represents.
 #include <stdio.h>
 void Temperatures(double f);
 
@@ -16,8 +18,8 @@ int main(void)
 void Temperatures(double f)
 {
     const double CEL_TO_KEL_COUNT = 273.16;
-    const double FAHR_TO_CEL_SCALE = 5.0 / 9.0;
     const double FAHR_TO_CEL_COUNT = -32.0;
+    const double FAHR_TO_CEL_SCALE = 5.0 / 9.0;
 
     double c = (f + FAHR_TO_CEL_COUNT) * FAHR_TO_CEL_SCALE;
     double k = c + CEL_TO_KEL_COUNT;
