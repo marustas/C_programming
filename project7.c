2d7a23a1c992f6b07fc1cf1b65ee591a78554ca2
Stanislau Marudau <marudov.stas@gmail.com>
2021-09-28 17:30:00 +0300

    project7 created

--- project7.c
+++ project7.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(void){
-    printf("");
-    
+int main(void)
+{
+    int category = 0;
+    if (category = 0)
+    {
+        printf("Category is zero\n");
+    }
+    int year = 2000;
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+    {
+        printf("The year of %d is a leap year.\n", year);
+    } else{
+       printf("The year of %d is not a leap year.\n", year); 
+    }
 }
