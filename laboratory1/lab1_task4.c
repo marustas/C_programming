011ffb42e2bbe3d38fa51bc2be403f17591829d4
Stanislau Marudau <marudov.stas@gmail.com>
2021-09-28 16:47:43 +0300

    Task4 improved

--- laboratory1/lab1_task4.c
+++ laboratory1/lab1_task4.c
@@ -2,6 +2,9 @@
 // Fractional centimeters and inches should be allowed, and the program should allow the user to continue entering heights until a nonpositive value is entered.
 #include <stdio.h>
 #include <math.h>
+#define INCH_SCALE_1 393701
+#define INCH_SCALE_2 1000000
+#define FEET_SCALE 12
 int main(void)
 {
   int c;
@@ -10,9 +13,9 @@ int main(void)
   {
     printf("Enter a height in centimeters: ");
     scanf("%lf", &a);
-    double b = a * 393701 / 1000000;
-    c = b / 12;
-    double d = b - 12 * c;
-    printf("%.1lf is equal to %d feet and %.1lf inches\n", a, c, d);
+    double b = a * INCH_SCALE_1 / INCH_SCALE_2;
+    c = b / FEET_SCALE;
+    double d = b - FEET_SCALE * c;
+    printf("%.1lf cm is equal to %d feet and %.1lf inches\n", a, c, d);
   } while (a > 0);
 }
