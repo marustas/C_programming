4a0c1580ab1251ddf33a1fc1107b48fe653a9984
Stanislau Marudau <marudov.stas@gmail.com>
2021-09-30 09:56:00 +0300

    Project8 updated

--- project8.c
+++ project8.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> // utility library
 #include <time.h>
+#define SIZE 7
 int main(void)
 {
     /* printf("RAND_MAX: %u\n", RAND_MAX);
@@ -13,14 +14,21 @@ int main(void)
     printf("%ld\n", time(0));*/
     // produce ramdom numbers [1,6]
     unsigned ntries = 60000;
-    unsigned nthrows[7] = {0};
+    unsigned nthrows[SIZE] = {0};
     for (size_t i = 0; i < ntries; ++i)
     {
         unsigned dice = rand() % 6 + 1;
         //printf("%6d\n", dice);
         ++nthrows[dice];
     }
-    for (size_t i = 0; i < 7; ++i){
-printf("%zu: %7u\n", i, nthrows[i]);
+    int output_field_len=0;
+    while (ntries > 0)
+    {
+        ntries /= 10;
+        ++output_field_len;
+    }
+    for (size_t i = 1; i < SIZE; ++i)
+    {
+        printf("%zu: %*u\n", i, output_field_len, nthrows[i]);
     }
 }
