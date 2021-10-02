9fbde0db44e84d3fe4c2fc42878c7940355b9637
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-02 13:35:13 +0300

    Task6 comments added

--- laboratory1/lab1_task6.c
+++ laboratory1/lab1_task6.c
@@ -1,4 +1,5 @@
-/* Now modify the program of Programming Exercise 5 so that it computes the sum of the squares of the integers. 
+/*
+Now modify the program of Programming Exercise 5 so that it computes the sum of the squares of the integers. 
 (If you prefer, how much money you receive if you get $1 the first day, $4 the second day, $9 the third day, and so on. This looks like a much better deal!)
 */
 #include <stdio.h>
