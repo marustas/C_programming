641481df41120133ebca30ce4dc9254692da3fa0
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-02 10:25:32 +0300

    Magic numbers terminated

--- laboratory1/lab1_task2.c
+++ laboratory1/lab1_task2.c
@@ -1,12 +1,13 @@
 //Write a program that asks for an integer and then prints all the integers from (and including) that value up to (and including) a value larger by 10. (That is, if the input is 5, the output runs from 5 to 15.)
 // Be sure to separate each output value by a space or tab or newline.
 #include <stdio.h>
+#define NUMBERS_SCALE 10
 int main(void)
 {
     int a, b;
     printf("Enter your variable: ");
     scanf("%d", &a);
-    b = a + 10;
+    b = a + NUMBERS_SCALE;
     for (a; a <= b; ++a)
     {
         printf("%d\n", a);
