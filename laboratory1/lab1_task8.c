cd16faeb38732a94a4e14aaa780fbfbd850b1a19
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-02 13:24:29 +0300

    Task8 code refactoring

--- laboratory1/lab1_task8.c
+++ laboratory1/lab1_task8.c
@@ -1,22 +1,24 @@
 /*
 Write a program that displays the results of applying the modulus operation.
-The user should first enter an integer to be used as the second operand, which will then remain unchanged.
-Then the user enters the numbers for which the modulus will be computed, terminating the process by entering 0 or less.
- */
+ The user should first enter an integer to be used as the second operand, which will then remain unchanged. 
+ Then the user enters the numbers for which the modulus will be computed, terminating the process by entering 0 or less.
+*/
 #include <stdio.h>
 int main(void)
 {
-    int a, b, output;
-    printf("Please enter an integer to serve as the second operand: ");
+    int a, b, c;
+    printf("Enter an integer to serve as the second operand: ");
     scanf("%d", &b);
-    do
+    printf("Now enter the first operand: ");
+    scanf("%d", &a);
+    while (a > 0)
     {
+        c = a % b;
+        printf("%d %% %d is %d\n", a, b, c);
 
-        printf("Now enter the first operand: ");
+        printf("Enter next number for first operand (<= 0 to quit): ");
         scanf("%d", &a);
-        output = a % b;
-        printf("Remainder of %d and %d is = %d\n", a, b, output);
-    } while (a > 0);
-
+    }
+    printf("Done");
     return 0;
 }
