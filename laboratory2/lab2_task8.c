72287de708831603af978ef7638cb44dc64250c9
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-05 16:25:12 +0300

    Task8 and Task11 completed

---
+++ laboratory2/lab2_task8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float a, b, c;
+	printf("Enter two floating-point numbers: ");
+	while (scanf(" %f %f", &a, &b) == 2)
+	{
+		c = (a - b) / (a * b);
+		printf("(%.3f - %.3f)/(%.3f * %.3f) = %.3f\n", a, b, a, b, c);
+		printf("Enter two floating-point numbers (enter non-numeric to quit): ");
+	}
+
+	return 0;
+}
