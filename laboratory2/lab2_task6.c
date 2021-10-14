9e1cee33cdb5e84e54a4569f5561bfd8d00756af
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-14 22:14:14 +0300

    Task 11 solved

---
+++ laboratory2/lab2_task6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long upper_limit = -1, lower_limit=0;
+	int reads;
+
+	printf("This program prints a table of integers with their "
+		   "squares and cubes.\n");
+	do
+	{	
+		printf("Enter lower and upper integer limits of the table: ");
+		reads = scanf("%ld%ld", &lower_limit, &upper_limit);
+		if (reads != 2)
+		{
+			while (getchar() != '\n') ; 
+		}
+	} while (lower_limit > upper_limit); 
+	printf("\n");
+
+	printf(" Integer       |Squared integer| Cubed integer \n");
+	printf("---------------|---------------|---------------\n");
+	for (long int i = lower_limit; i <= upper_limit; i++)
+	{
+		printf(" %-14ld| %-14ld| %-14ld\n", i, i * i, i * i * i);		
+	}
+	printf("\n");
+
+	return 0;
+}
