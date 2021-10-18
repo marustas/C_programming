80862d25f44380860c9b82aa75b009628541f81a
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-18 18:16:28 +0300

    Task8 code refactoring

--- laboratory2/lab2_task8.c
+++ laboratory2/lab2_task8.c
@@ -3,11 +3,13 @@
 int main(void)
 {
 	float a, b, c;
+	
 	printf("Enter two floating-point numbers: ");
 	
 	while (scanf(" %f %f", &a, &b) == 2)
 	{
 		c = (a - b) / (a * b);
+
 		printf("(%.3f - %.3f)/(%.3f * %.3f) = %.3f\n", a, b, a, b, c);
 		printf("Enter two floating-point numbers (enter character to quit): ");
 	}
