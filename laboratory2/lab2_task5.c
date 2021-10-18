383d5b9c2019a0110f6280abe0f7893fc251f09a
Stanislau Marudau <marudov.stas@gmail.com>
2021-10-18 18:12:27 +0300

    Code refactoring

--- laboratory2/lab2_task5.c
+++ laboratory2/lab2_task5.c
@@ -4,41 +4,40 @@ void print_spaces(unsigned int n);
 
 int main(void)
 {
-    char uppercase_letter;
-    char a, b;
+	char uppercase_letter;
+	char a, b;
 
-    do
-    {
-        printf("Enter an uppercase letter: ");
-        scanf(" %c", &uppercase_letter);
-    } while (uppercase_letter < 'A' || 'Z' < uppercase_letter);
+	do
+	{
+		printf("Enter an uppercase letter: ");
+		scanf(" %c", &uppercase_letter);
+	} while (uppercase_letter < 'A' || 'Z' < uppercase_letter);
 
-    for (a = 'A'; a <= uppercase_letter; a++)
-    {
+	for(a = 'A'; a <= uppercase_letter; a++)
+	{
+		print_spaces(uppercase_letter - a);
 
-        print_spaces(uppercase_letter - a);
+		for (b = 'A'; b < a; b++)
+		{
+			printf("%c", b);
+		}
 
-        for (b = 'A'; b < a; b++)
-        {
-            printf("%c", b);
-        }
-        while ('A' <= b)
-        {
-            b--;
-            printf("%c", b);
-        }
+		for (; 'A' <= b; b--)
+		{
+			printf("%c", b);
+		}
 
-        print_spaces(uppercase_letter - a);
-        printf("\n");
-    }
+		print_spaces(uppercase_letter - a);
+		printf("\n");
+	}
 
-    return 0;
+	return 0;
 }
 
 void print_spaces(unsigned int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        printf(" ");
-    }
+	for (int i = 0; i < n; i++)
+	{
+		printf(" ");
+	}
 }
