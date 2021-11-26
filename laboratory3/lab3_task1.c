0b775d02990d7863ddf705352095db03d1874719
Stanislau Marudau <marudov.stas@gmail.com>
2021-11-26 22:44:10 +0200

    Laboratory3 started and 1st task completed

---
+++ laboratory3/lab3_task1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+double min(double, double);
+
+int main(void)
+{
+    double x, y;
+
+    printf("Please enter the variables:\n");
+    while (scanf("%lf %lf", &x, &y) == 2)
+    {
+        if (x != y)
+        {
+            printf("The minimum of %lf and %lf is %lf.\n", x, y, min(x, y));
+            printf("Please enter the variables:\n");
+        }
+        else
+        {
+            printf("The variables are equal, please try different values.\n");
+            printf("Please enter the variables:\n");
+        }
+    }
+    printf("Bye.\n");
+
+    return 0;
+}
+
+double min(double x, double y)
+{
+    return x < y ? x : y;
+}
