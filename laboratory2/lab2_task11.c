a6c3c4888c5595d95e9796fa9c3a1081ab5f733d
Stanislau Marudau <marudov.stas@gmail.com>
2022-01-12 13:08:26 +0200

    Code refactoring

--- laboratory2/lab2_task11.c
+++ laboratory2/lab2_task11.c
@@ -2,20 +2,21 @@
 Write a program that reads eight integers into an array and then prints them in reverse order.
 */
 #include <stdio.h>
+# define SIZE 8
 
 int main(void)
 {
-	int int_array[8];
+	int int_array[SIZE];
 	int i; 
 
 	printf("Enter 8 integers:\n");
 
-	for (i = 0; i < 8; i++) 
+	for (i = 0; i < SIZE; i++) 
 	{
 		scanf("%d", &int_array[i]);
 	}
 
-	for (i--; i >= 0; i--) 
+	for (i=SIZE-1; i >=0; i--) 
 	{
 		printf("%d ", int_array[i]);
 	}
