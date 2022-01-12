2629739a7973e516ad77ff13b387dadde3f4dd67
Stanislau Marudau <marudov.stas@gmail.com>
2022-01-12 18:52:17 +0200

    Perfect pyramid pattern created

---
+++ pyramid_pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int rows;
+    printf("Enter the character you want to print: ");
+    scanf("%c", &ch);
+    printf("Enter the number of rows you want to have: ");
+    scanf("%d", &rows);
+    for (int i = 0; i <= rows; i++)
+    {
+          for (int spaces = i; spaces < rows; spaces++)
+        {
+            printf(" ");
+        }
+        for (int x = 0; x <= i; x++)
+        {
+            printf("%c", ch);
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
